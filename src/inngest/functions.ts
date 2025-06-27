2a8d92e29a2e2392d780568a78f4ff3ac4cd3809
unknown <firas.jerbi.engineer@gmail.com>
2025-06-27 22:59:44 +0200

    Sandbox Initial Setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
 import { createAgent, gemini } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
-
+  async ({ event, step}) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("icode-nextjs-test-2");
+      return sandbox.sandboxId;
+    })
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert NextJs Developer.  You write readable maintanable code. You write simple NextJs Snippets.",
@@ -17,6 +23,11 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet:${event.data.value}`,
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000)
+      return `https://${host}`;
+    })
+    return { output, sandboxUrl };
   },
 );
